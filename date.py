8f26f6f8074e78c2173490080c7a9b35c5c93251
lucifer950924 <amaiti.9509@gmail.com>
2024-05-10 23:42:32 +0530

    date_01

---
+++ date.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By as by
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from datetime import datetime as dt
+# //td[@class="dt-right dt-body-right"]
+driver = webdriver.Chrome()
+driver.get('https://datatables.net/')
+driver.maximize_window()
+wait(driver,5).until(EC.presence_of_element_located((by.XPATH,'//h1[contains(@data-anchor,"Add-advanced-interaction-controls")]')))
+# time.sleep(1)
+# element = lambda x : '(//td[@class="dt-right dt-body-right"])' + [x]
+# for i in range(11):
+    
+wait(driver,5).until(EC.presence_of_element_located((by.XPATH,'//td[@class="dt-right dt-body-right"]')))
+date = driver.find_element(by.XPATH,'//td[@class="dt-right dt-body-right"]').text
+print(date)
+
+dat = dt.strptime(date,"%d-%m-%y")
+print(dat)
