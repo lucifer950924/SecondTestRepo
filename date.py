212c3742adb3720fdd01f0211b517b8f2481b845
lucifer950924 <amaiti.9509@gmail.com>
2024-05-11 11:13:02 +0530

    DAte_02

--- date.py
+++ date.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By as by
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from datetime import datetime as dt
+from dateutil import parser
 # //td[@class="dt-right dt-body-right"]
 driver = webdriver.Chrome()
 driver.get('https://datatables.net/')
@@ -15,6 +16,6 @@ wait(driver,5).until(EC.presence_of_element_located((by.XPATH,'//h1[contains(@da
 wait(driver,5).until(EC.presence_of_element_located((by.XPATH,'//td[@class="dt-right dt-body-right"]')))
 date = driver.find_element(by.XPATH,'//td[@class="dt-right dt-body-right"]').text
 print(date)
-
-dat = dt.strptime(date,"%d-%m-%y")
-print(dat)
+date_obj = parser.parse(date).strftime("%d-%m-%Y")
+# dat = dt.strptime(date,"%d-%m-%y")
+print(date_obj)
